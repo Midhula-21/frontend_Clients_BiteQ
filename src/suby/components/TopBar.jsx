d6780a6d88f32f8c8e5cd760ed9797b8a48e3f48
Midhula-21 <midhula321@gmail..com>
2025-04-08 23:24:54 +0530

    Iitial commit

--- src/suby/components/TopBar.jsx
+++ src/suby/components/TopBar.jsx
@@ -5,12 +5,15 @@ const TopBar = () => {
   return (
    <section className="topBarSection">
         <div className="companyTitle">
+            <Link>
+            <img src= "/assets/logo/logo1.jpeg" alt="logo" cl/>
+            </Link>
             <Link to='/' className='link'>
-            <h2>QUICK</h2>
+            <h2>BiteQ</h2>
             </Link>
         </div>
         <div className="searchBar">
-            <input type="text" placeholder='Search...' />
+            <input type="text" placeholder='Search...' style={{ width: '500px'}}/>
         </div>
         <div className="userAuth">
             Login / SignUp
